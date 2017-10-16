0dcdb6eaf0e0c901f37ff8603adb16b22bc90676
MaSommer <martisom@stud.ntnu.no>
2017-10-16 10:34:53 +0200

    Check

--- bioAlgJavaProject/src/Main.java
+++ bioAlgJavaProject/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         DNAFile file = new DNAFile("s_3_sequence_1M.txt");
         ArrayList<String> dnaStrings = file.getDnaStrings();
         System.out.println(dnaStrings.size());
+        int a = 2;
     }
 
 }
